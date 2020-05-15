89799deebf3f3a1957b6a9abae8837a1813c4171
Artyom Burkan <artyom.burkan@gmail.com>
2020-05-15 15:13:34 +0700

    Init Weather app

---
+++ weather/weather/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by Artyom Burkan on 14.05.2020.
+//  Copyright © 2020 Artyom Burkan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
